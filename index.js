41a02e41443523669fe3810d79301e58a1075279
dee-d-dev <adedigbaadedotunemmanuel@gmail.com>
2022-03-07 23:42:07 +0100

    data relations

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const items = [
     price: 42.44,
     image: "img-1",
     onSale: false,
+    category_id: "c01b1ff4-f894-4ef2-b27a-22aacc2fca70",
   },
   {
     id: "c2af9adc-d0b8-4d44-871f-cef66f86f7f6",
@@ -18,6 +19,7 @@ const items = [
     price: 53.5,
     image: "img-2",
     onSale: false,
+    category_id: "c01b1ff4-f894-4ef2-b27a-22aacc2fca70",
   },
   {
     id: "2c931e7e-510f-49e5-aed6-d6b44087e5a1",
@@ -27,6 +29,7 @@ const items = [
     price: 1.33,
     image: "img-3",
     onSale: true,
+    category_id: "c01b1ff4-f894-4ef2-b27a-22aacc2fca70",
   },
   {
     id: "404daf2a-9b97-4b99-b9af-614d07f818d7",
@@ -36,6 +39,7 @@ const items = [
     price: 332,
     image: "img-4",
     onSale: false,
+    category_id: "34115aac-0ff5-4859-8f43-10e8db23602b",
   },
   {
     id: "6379c436-9fad-4b3f-a427-2d7241f5c1b1",
@@ -45,6 +49,7 @@ const items = [
     price: 23.11,
     image: "img-5",
     onSale: true,
+    category_id: "34115aac-0ff5-4859-8f43-10e8db23602b",
   },
   {
     id: "f01bcdec-6783-464e-8f9e-8416830f7569",
@@ -54,6 +59,7 @@ const items = [
     price: 59.99,
     image: "img-6",
     onSale: true,
+    category_id: "d914aec0-25b2-4103-9ed8-225d39018d1d",
   },
   {
     id: "a4824a31-5c83-42af-8c1b-6e2461aae1ef",
@@ -63,6 +69,7 @@ const items = [
     price: 427.44,
     image: "img-7",
     onSale: false,
+    category_id: "d914aec0-25b2-4103-9ed8-225d39018d1d",
   },
   {
     id: "b553085a-a7e0-4c9b-8a12-f971919c3683",
@@ -72,6 +79,7 @@ const items = [
     price: 77.0,
     image: "img-8",
     onSale: true,
+    category_id: "d914aec0-25b2-4103-9ed8-225d39018d1d",
   },
   {
     // 47bf3941-9c8b-42c0-9c72-7f3985492a5b
@@ -82,6 +90,7 @@ const items = [
     price: 93.44,
     image: "img-9",
     onSale: false,
+    category_id: "d914aec0-25b2-4103-9ed8-225d39018d1d",
   },
 ];
 
@@ -125,6 +134,7 @@ const typeDefs = gql`
   type Category {
     id: ID
     name: String
+    products: [Product]
   }
 `;
 
@@ -157,6 +167,17 @@ const resolvers = {
 
       if (!found) return null;
       return found;
+    },
+  },
+  Category: {
+    products: (parent, args, ctx) => {
+      const queery = items.filter((item) => {
+        return item.category_id === parent.id;
+      });
+
+      if (!queery) return null;
+
+      return queery;
     },
   },
 };
