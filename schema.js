b9ff599807d362fc2f1150cdf01c4805d6f1f16d
dee-d-dev <adedigbaadedotunemmanuel@gmail.com>
2022-03-08 12:52:49 +0100

    implementing separation of concerns

---
+++ schema.js
@@ -0,0 +1,31 @@
+const { gql } = require("apollo-server");
+exports.typeDefs = gql`
+  type Query {
+    hello: String
+    numberOfAnimals: Int
+    price: Float
+    isExpensive: Boolean
+    Shoes: [String]
+    products: [Product!]!
+    product(id: ID!): Product
+    categories: [Category!]!
+    category(id: ID!): Category!
+  }
+  type Product {
+    id: ID!
+    name: String!
+    description: String!
+    quantity: Int!
+    price: Float!
+    image: String!
+    onSale: Boolean!
+    category: Category
+    category_id: ID!
+  }
+
+  type Category {
+    id: ID
+    name: String
+    products: [Product]
+  }
+`;
